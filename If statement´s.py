630f5d6920aaae1fd3895b543ac6ca2566f3032c
DELLD <dennis_pascua@unitec.edu>
2016-03-26 17:44:29 -0600

    Programa que pregunt si el número es ODD o EVEN

--- If statement´s.py
+++ If statement´s.py
@@ -5,6 +5,28 @@ if (a==10):
 	print(a)
 	print("This is true")
 
-if (b=!a):	
+#Else
+c = 30
+if (a==10):
+	print(a)
+elif(b==20):
 	print(b)
-	print("This is a lie")
+else:
+	print(c)
+
+#Elif
+if (a==20):
+	print(a)
+elif(b==30):
+	print(b)
+else:
+	print(c)
+
+#Printing the three of them
+if (a==10):
+	print(a)
+	if (b==20):
+		print(b)
+		if(c==30):
+			print(c)
+#Spaces are very important's due to identation's
